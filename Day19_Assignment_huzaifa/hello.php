e3fb333bc8265faffd2e6f258596ce95ca97e1c5
huzaifashaikh-56 <hs7030342@gmail.com>
2025-06-22 20:14:06 +0530

    Update hello.php

--- Day19_Assignment_huzaifa/hello.php
+++ Day19_Assignment_huzaifa/hello.php
@@ -1,1 +1,24 @@
+<?php
+// Single-line comment
 
+/*
+Multi-line comment
+This is a sample PHP script
+*/
+
+// Using echo and print
+echo "Hello from PHP using echo!<br>";
+print "Hello from PHP using print!<br>";
+
+// Variable declarations
+$name = "Huzaifa";     // String
+$age = 21;             // Integer
+$height = 5.9;         // Float
+$isStudent = true;     // Boolean
+
+// Displaying variables
+echo "Name: $name<br>";
+echo "Age: $age<br>";
+echo "Height: $height<br>";
+echo "Student: " . ($isStudent ? "Yes" : "No") . "<br>";
+?>
